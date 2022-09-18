cd2862d025c6e289e94c7ebedc7b82d517de1e51
Melvin Santos <itsmelvinsantos@gmail.com>
2022-09-18 17:33:00 -0400

    Simple Flashcards lab done

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var bacgroundView: UIView!
+    
+    @IBOutlet weak var answerCard: UILabel!
+    @IBOutlet weak var questionCard: UILabel!
+    @IBOutlet weak var imageLogo: UIImageView!
+    
+    @IBOutlet weak var germanFlag: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
+        self.bacgroundView.layer.cornerRadius = 50
+        self.bacgroundView.layer.masksToBounds = true
+        
+        self.questionCard.layer.cornerRadius = 50
+        self.questionCard.layer.masksToBounds = true
+        
+        self.answerCard.layer.cornerRadius = 50
+        self.answerCard.layer.masksToBounds = true
+        
+        self.germanFlag.layer.cornerRadius = 10
+        self.germanFlag.layer.masksToBounds = true
+        
+        germanFlag.isHidden = true
+        
+
     }
-
-
+    
+    
+    @IBAction func tappedFlashcard(_ sender: Any) {
+        questionCard.isHidden = true
+        germanFlag.isHidden = false
+    }
+    
+    
 }
 
